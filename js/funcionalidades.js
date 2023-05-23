c993d1958113759156ec37537735828b62ec9bb6
Francisco <paco19956@hotmail.com>
2023-05-23 16:30:06 -0300

    Portfolio v2.0.4 - hice retoques al menu hamburguesa

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,3 +1,5 @@
+/* MENU HAMBURGUESA EN MOBILE */
+
 let boton_menu = document.querySelector('.boton-menu');
 let menu = document.querySelector('.menu-hamburguesa ul');
 
@@ -5,3 +7,31 @@ let menu = document.querySelector('.menu-hamburguesa ul');
 boton_menu.addEventListener('click', function() {
     menu.classList.toggle('visible');
 })
+
+document.addEventListener('click', function(e) {
+    if (menu.classList == 'visible') {
+        switch (e.target.classList.value) {
+            case 'boton-menu':
+            case 'imagen-menu':
+                break;
+
+            default:
+                menu.classList.toggle('visible');
+                break;
+        }
+    }
+})
+
+
+/* BOTON PARA MANDAR MAIL */
+
+/* por ahi con un simple mailto: en el html sale */
+
+
+/* DESCARGA DEL CV */
+
+
+/* MODO LIGHT/DARK */
+
+
+/* EASTER EGG */
