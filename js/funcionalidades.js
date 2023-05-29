bbb36f9bfb9016317a6774f73d65951d23276f91
Francisco Garcia Huidobro <paco19956@hotmail.com>
2023-05-29 04:35:32 -0300

    Portfolio v3.0.0 - agregadas todas las funcionalidades. Descarga de CV, variar entre light mode y dark mode, easter egg secreto

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -12,7 +12,8 @@ document.addEventListener('click', function(e) {
     if (menu.classList == 'visible') {
         switch (e.target.classList.value) {
             case 'boton-menu':
-            case 'imagen-menu':
+            case 'menu-mobile':
+            case 'icon-modo':
                 break;
 
             default:
@@ -23,15 +24,56 @@ document.addEventListener('click', function(e) {
 })
 
 
-/* BOTON PARA MANDAR MAIL */
-
-/* por ahi con un simple mailto: en el html sale */
-
-
-/* DESCARGA DEL CV */
-
-
 /* MODO LIGHT/DARK */
+
+let boton = document.querySelector('.boton-modo');
+let indice = boton.classList.value.indexOf(" ");
+
+
+boton.addEventListener('click', function() {
+    let modo = boton.classList.value.slice(indice+1);
+    let elementos = document.querySelectorAll('.' + modo);
+    elementos.forEach(el => {
+        el.classList.remove(modo);
+        if (modo == 'dark-mode') {
+            el.classList.add('light-mode');
+        } else {
+            el.classList.add('dark-mode');
+        }
+    })
+})
 
 
 /* EASTER EGG */
+
+let cv = document.querySelector('.grid-item3');
+let conocimientos = document.querySelectorAll('.contenedor-conocimientos ul li');
+let proyectos = document.querySelectorAll('.proyecto');
+let cara = document.querySelector('.contenedor-sorpresa img');
+let musica = document.querySelector('audio');
+
+
+cara.addEventListener('click', function() {
+    if (cara.classList.value != 'sorpresa') {
+        /* ESTO LO PUEDO DEFINIR CON UNA FUNCION */
+        cv.classList.add('sorpresa');
+        conocimientos.forEach(li => {
+            li.classList.add('sorpresa');
+        });
+        proyectos.forEach(proyecto => {
+            proyecto.classList.add('sorpresa');
+        });
+        cara.classList.add('sorpresa');
+        musica.play();
+        setTimeout(function() {
+            cv.classList.remove('sorpresa');
+            conocimientos.forEach(li => {
+                li.classList.remove('sorpresa');
+            });
+            proyectos.forEach(proyecto => {
+                proyecto.classList.remove('sorpresa');
+            });
+            cara.classList.remove('sorpresa');
+        }, 16000);
+    }
+})
